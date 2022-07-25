c614a5d383fd75979bd8a3c0f33de40bfc3a3b61
Gaurav Patel <gaurav.gkp29@gmail.com>
2022-07-25 11:36:52 +0530

    changes

--- routes/customer.js
+++ routes/customer.js
@@ -58,50 +58,6 @@ router.post('/update', async(req, res) => {
 });
 
 // APIS
-router.get('/check-price', async(req, res) => {
-    try {
-        req.query = await encryptDecryptHandler.decryptJson(req.query.encrypt_data)
-        if (!req.query.customer_id || !req.query.vehicle_id || !req.query.delivery_option_id) {
-            jsonResponse(res, responseCodes.BadRequest, errors(labels.LBL_MISSING_PARAMETERS[config.default_language], responseCodes.BadRequest), null)
-            return
-        }
-        if (!req.query.pickup_address || !req.query.delivery_address) {
-            jsonResponse(res, responseCodes.BadRequest, errors(labels.LBL_MISSING_PARAMETERS[config.default_language], responseCodes.BadRequest), null)
-            return
-        }
-        let response = await customerHandler.checkPrice(req.query);
-        jsonResponse(res, responseCodes.OK, null, response);
-    } catch (error) {
-        jsonResponse(res, error.code, error, null);
-    }
-});
-
-router.post('/create-job', async(req, res) => {
-    try {
-        req.body = await encryptDecryptHandler.decryptJson(req.body.encrypt_data)
-        req.body.time_zone = config.time_zone
-        if(req.headers.time_zone){
-            req.body.time_zone = req.headers.time_zone
-        }
-        if (!req.body.customer_id || !req.body.vehicle_id || !req.body.delivery_option_id || !req.body.pickup_from || !req.body.item_name || !req.body.item_desc) {
-            jsonResponse(res, responseCodes.BadRequest, errors(labels.LBL_MISSING_PARAMETERS[config.default_language], responseCodes.BadRequest), null)
-            return
-        }
-        if (!req.body.pickup_address || !req.body.pickup_latitude || !req.body.pickup_longitude) {
-            jsonResponse(res, responseCodes.BadRequest, errors(labels.LBL_MISSING_PARAMETERS[config.default_language], responseCodes.BadRequest), null)
-            return
-        }
-        if (!req.body.delivery_address || !req.body.delivery_latitude || !req.body.delivery_longitude) {
-            jsonResponse(res, responseCodes.BadRequest, errors(labels.LBL_MISSING_PARAMETERS[config.default_language], responseCodes.BadRequest), null)
-            return
-        }
-        let response = await customerHandler.createJob(req.body);
-        jsonResponse(res, responseCodes.OK, null, response);
-    } catch (error) {
-        jsonResponse(res, error.code, error, null);
-    }
-});
-
 router.post('/signup', async(req, res) => {
     try {
         req.body.time_zone = config.time_zone
@@ -195,6 +151,54 @@ router.post('/logout-delete', async(req, res) => {
             return
         }
         let response = await customerHandler.logoutDelete(req.body);
+        jsonResponse(res, responseCodes.OK, null, response);
+    } catch (error) {
+        jsonResponse(res, error.code, error, null);
+    }
+});
+
+router.get('/check-price', async(req, res) => {
+    try {
+        req.query = await encryptDecryptHandler.decryptJson(req.query.encrypt_data)
+        if (!req.query.customer_id || !req.query.vehicle_id || !req.query.delivery_option_id) {
+            jsonResponse(res, responseCodes.BadRequest, errors(labels.LBL_MISSING_PARAMETERS[config.default_language], responseCodes.BadRequest), null)
+            return
+        }
+        if (!req.query.pickup_address || !req.query.delivery_address) {
+            jsonResponse(res, responseCodes.BadRequest, errors(labels.LBL_MISSING_PARAMETERS[config.default_language], responseCodes.BadRequest), null)
+            return
+        }
+        let response = await customerHandler.checkPrice(req.query);
+        jsonResponse(res, responseCodes.OK, null, response);
+    } catch (error) {
+        jsonResponse(res, error.code, error, null);
+    }
+});
+
+router.post('/create-job', async(req, res) => {
+    try {
+        req.body = await encryptDecryptHandler.decryptJson(req.body.encrypt_data)
+        req.body.time_zone = config.time_zone
+        if(req.headers.time_zone){
+            req.body.time_zone = req.headers.time_zone
+        }
+        if (!req.body.customer_id || !req.body.vehicle_id || !req.body.delivery_option_id || !req.body.pickup_from || !req.body.item_name || !req.body.item_desc) {
+            jsonResponse(res, responseCodes.BadRequest, errors(labels.LBL_MISSING_PARAMETERS[config.default_language], responseCodes.BadRequest), null)
+            return
+        }
+        if (!req.body.pickup_address || !req.body.pickup_latitude || !req.body.pickup_longitude) {
+            jsonResponse(res, responseCodes.BadRequest, errors(labels.LBL_MISSING_PARAMETERS[config.default_language], responseCodes.BadRequest), null)
+            return
+        }
+        if (!req.body.delivery_address || !req.body.delivery_latitude || !req.body.delivery_longitude) {
+            jsonResponse(res, responseCodes.BadRequest, errors(labels.LBL_MISSING_PARAMETERS[config.default_language], responseCodes.BadRequest), null)
+            return
+        }
+        if (!req.body.total || !req.body.amount_pay) {
+            jsonResponse(res, responseCodes.BadRequest, errors(labels.LBL_MISSING_PARAMETERS[config.default_language], responseCodes.BadRequest), null)
+            return
+        }
+        let response = await customerHandler.createJob(req.body);
         jsonResponse(res, responseCodes.OK, null, response);
     } catch (error) {
         jsonResponse(res, error.code, error, null);
