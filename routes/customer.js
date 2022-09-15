9e0fb5cd3ec8ad60b8279aea60c22012385b5cb8
Gaurav Patel <gaurav.gkp29@gmail.com>
2022-09-15 12:46:13 +0530

    changes

--- routes/customer.js
+++ routes/customer.js
@@ -64,7 +64,7 @@ router.post('/signup', async(req, res) => {
         if(req.headers.time_zone){
             req.body.time_zone = req.headers.time_zone
         }
-        if (!req.body.name || !req.body.mobile_country_code || !req.body.mobile || !req.body.email || !req.body.password || !req.files.profile_photo) {
+        if (!req.body.name || !req.body.mobile_country_code || !req.body.mobile || !req.body.email || !req.body.password) {
             jsonResponse(res, responseCodes.BadRequest, errors(labels.LBL_MISSING_PARAMETERS[config.default_language], responseCodes.BadRequest), null)
             return
         }
