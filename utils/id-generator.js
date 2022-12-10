8525317752a03ad6b923988f123d58df9c653527
Gaurav Patel <gaurav.gkp29@gmail.com>
2022-12-10 17:59:15 +0530

    changes

--- utils/id-generator.js
+++ utils/id-generator.js
@@ -1,11 +1,12 @@
 const moment = require('moment');
 const randomString = require('random-string');
-const { Entropy } = require('entropy-string')
+const { Entropy, charset8 } = require('entropy-string')
 const entropy = new Entropy()
 
 const generateId = (label = '')=> {
     return new Promise((resolve) => {
-        resolve(`${label}${moment().unix()}${Math.floor((Math.random() * 99) + 11)}`);
+        //resolve(`${label}${moment().unix()}${Math.floor((Math.random() * 99) + 11)}`);
+        resolve(`${label}${moment().unix()}${entropy.smallID()}`);
         return;
     })
 };
