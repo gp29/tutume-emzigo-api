bc66be61bbc325538170f2739b2e5430e72bad43
Gaurav Patel <gaurav.gkp29@gmail.com>
2023-06-14 16:28:39 +0530

    chaneg

--- routes/petrol-modules/money-agent.js
+++ routes/petrol-modules/money-agent.js
@@ -36,7 +36,7 @@ router.get('/profile', async(req, res) => {
         if(req.headers.time_zone){
             req.query.time_zone = req.headers.time_zone
         }
-        if (!req.query.rider_id) {
+        if (!req.query.user_id) {
             jsonResponse(res, responseCodes.BadRequest, errors(labels.LBL_MISSING_PARAMETERS[config.default_language], responseCodes.BadRequest), null)
             return
         }
@@ -79,6 +79,25 @@ router.post('/pay-rider-installment', async(req, res) => {
             return
         }
         let response = await handler.payRiderInstallment(req.body);
+        jsonResponse(res, responseCodes.OK, null, response);
+    } catch (error) {
+        console.log(error)
+        jsonResponse(res, error.code, error, null);
+    }
+});
+
+router.get('/recent-activities', async(req, res) => {
+    try {
+        req.query = await encryptDecryptHandler.decryptJson(req.query.encrypt_data)
+        req.query.time_zone = config.time_zone
+        if(req.headers.time_zone){
+            req.query.time_zone = req.headers.time_zone
+        }
+        if (!req.query.user_id) {
+            jsonResponse(res, responseCodes.BadRequest, errors(labels.LBL_MISSING_PARAMETERS[config.default_language], responseCodes.BadRequest), null)
+            return
+        }
+        let response = await handler.recentActivities(req.query);
         jsonResponse(res, responseCodes.OK, null, response);
     } catch (error) {
         console.log(error)
